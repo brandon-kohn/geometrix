a5266747d0489d4b55d63f1252f996abf15df82f
blkohn <blkohn@0866cac1-8f29-455d-85ba-443ca8371b41>
2008-10-21 21:56:56 +0000

    Modified the indexed_access_traits type to specialize based on the underlying type.

--- geometry/indexed_access_traits.hpp
+++ geometry/indexed_access_traits.hpp
@@ -29,7 +29,7 @@ template <typename Sequence>
 struct has_run_time_access : boost::false_type{};
 
 //! \brief Default traits for an indexed access sequence.
-template < typename Sequence >
+template < typename Sequence, typename Enable = void >
 struct indexed_access_traits
 {
     typedef Sequence                                          sequence_type;
@@ -38,10 +38,25 @@ struct indexed_access_traits
     typedef typename has_compile_time_access< sequence_type > has_compile_time_access_type;
     typedef typename has_run_time_access< sequence_type >     has_run_time_access_type;
 
-    //! dimension_access needs to be specialized on point type up to Sequence's dimension_type...    
-    //! This needs to support both runtime random access and compile time random access.
+    BOOST_MPL_ASSERT_MSG( 
+		  ( false )
+		, INDEXED_ACCESS_TRAITS_NOT_DEFINED
+		, (indexed_access_traits<Sequence>) );
+};
+
+//! \brief Specialization of indexed access traits for type which have both run-time and compile-time access.
+template < typename Sequence >
+struct indexed_access_traits< Sequence, typename boost::enable_if_c< has_compile_time_access< Sequence >::value && has_run_time_access< Sequence >::value >::type >
+{
+    typedef Sequence                                          sequence_type;
+    typedef typename sequence_type::value_type                indexed_type;
+    typedef typename sequence_type::dimension_type            dimension_type;
+    typedef typename has_compile_time_access< sequence_type > has_compile_time_access_type;
+    typedef typename has_run_time_access< sequence_type >     has_run_time_access_type;
+
+    //! \brief compile time access if available for the sequence.
     template <unsigned int Index>
-    static inline indexed_type get( const Sequence& sequence, typename boost::enable_if< has_compile_time_access_type >::type* dummy = 0 ) 
+    static inline typename indexed_type get( const Sequence& sequence ) 
     {
         BOOST_MPL_ASSERT_MSG
         (
@@ -53,11 +68,71 @@ struct indexed_access_traits
         return sequence.get<dimension_traits<Index>::value>();
     }
 
-    static inline indexed_type get( const Sequence& sequence, size_t index, typename boost::enable_if< has_run_time_access_type >::type* dummy = 0  ) 
+    //! \brief run-time access method if the sequence supports it.
+    static inline indexed_type get( const Sequence& sequence, size_t index  ) 
     {        
         assert( index >= 0 && index < dimension_type::value );		   
         return sequence[ index ];
     }
+};
+
+//! \brief Specialization of indexed access traits for type which have run-time access but no compile-time access.
+template < typename Sequence >
+struct indexed_access_traits< Sequence, typename boost::enable_if_c< !has_compile_time_access< Sequence >::value && has_run_time_access< Sequence >::value >::type >
+{
+    typedef Sequence                                          sequence_type;
+    typedef typename sequence_type::value_type                indexed_type;
+    typedef typename sequence_type::dimension_type            dimension_type;
+    typedef typename has_compile_time_access< sequence_type > has_compile_time_access_type;
+    typedef typename has_run_time_access< sequence_type >     has_run_time_access_type;
+
+    //! \brief compile time access if available for the sequence.
+    template <unsigned int Index>
+    static inline typename indexed_type get( const Sequence& sequence ) 
+    {
+        BOOST_MPL_ASSERT_MSG
+        (
+           ( dimension_traits< Index >::value >= 0 && dimension_traits< Index >::value < dimension_type::value )
+		   , INDEX_OUT_OF_BOUNDS
+		   , (Sequence)
+        );
+
+        return sequence[ Index ];
+    }
+
+    //! \brief run-time access method if the sequence supports it.
+    static inline indexed_type get( const Sequence& sequence, size_t index ) 
+    {        
+        assert( index >= 0 && index < dimension_type::value );		   
+        return sequence[ index ];
+    }
+};
+
+//! \brief Specialization of indexed access traits for type which have compile-time access, but no run-time access.
+template < typename Sequence >
+struct indexed_access_traits< Sequence, typename boost::enable_if_c< has_compile_time_access< Sequence >::value && !has_run_time_access< Sequence >::value >::type >
+{
+    typedef Sequence                                          sequence_type;
+    typedef typename sequence_type::value_type                indexed_type;
+    typedef typename sequence_type::dimension_type            dimension_type;
+    typedef typename has_compile_time_access< sequence_type > has_compile_time_access_type;
+    typedef typename has_run_time_access< sequence_type >     has_run_time_access_type;
+
+    //! \brief compile time access if available for the sequence.
+    template <unsigned int Index>
+    static inline typename indexed_type get( const Sequence& sequence ) 
+    {
+        BOOST_MPL_ASSERT_MSG
+        (
+           ( dimension_traits< Index >::value >= 0 && dimension_traits< Index >::value < dimension_type::value )
+		   , INDEX_OUT_OF_BOUNDS
+		   , (Sequence)
+        );
+
+        return sequence.get<dimension_traits<Index>::value>();
+    }
+
+    //! TODO: fake run-time with switch?
 };
 
 }}}//namespace boost::numeric::geometry;

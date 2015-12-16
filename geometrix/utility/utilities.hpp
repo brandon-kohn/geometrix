da7f97f5c8050282df2d5490943c1173acfc5a45
Brandon Kohn <blkohn@hotmail.com>
2015-12-16 11:05:45 -0500

    Fixed an include.

--- geometrix/utility/utilities.hpp
+++ geometrix/utility/utilities.hpp
@@ -1,5 +1,5 @@
 //
-//! Copyright © 2008-2011
+//! Copyright ï¿½ 2008-2011
 //! Brandon Kohn
 //
 //  Distributed under the Boost Software License, Version 1.0. (See
@@ -16,7 +16,7 @@
 #include <geometrix/primitive/segment_traits.hpp>
 #include <geometrix/arithmetic/vector.hpp>
 #include <geometrix/algebra/expression.hpp>
-#include <geometrix/algebra/cross_product.hpp>
+#include <geometrix/algebra/exterior_product.hpp>
 #include <boost/concept_check.hpp>
 #include <boost/numeric/conversion/cast.hpp>
 

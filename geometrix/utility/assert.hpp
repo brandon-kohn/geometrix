24292ef09d1c82fcd486d484dfc4464bc4aa9d3a
Brandon Kohn <blkohn@hotmail.com>
2015-12-24 10:32:48 -0500

    Fixed missing endifs.

--- geometrix/utility/assert.hpp
+++ geometrix/utility/assert.hpp
@@ -11,13 +11,16 @@
 
 //! Fix for used typedef in boost (which is fixed in versions beyond 1.58 I think.)
 #include <boost/static_assert.hpp>
+
 #if (defined(__apple_build_version__) && (__apple_build_version__ >= 7000000))
 #  ifdef BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
 #  undef BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
+#  endif
 #  define BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE __attribute__( (unused) )
 #else
 #  ifdef BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
 #  undef BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
+#  endif
 #  define BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
 #endif
 

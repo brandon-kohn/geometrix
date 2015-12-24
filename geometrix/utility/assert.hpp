be11f4b1b6a90402e29bbab89c2eadb8526e11a5
Brandon Kohn <blkohn@hotmail.com>
2015-12-24 10:27:39 -0500

    Attempt to patch in a warning fix for boost versions earlier than 1.58.

--- geometrix/utility/assert.hpp
+++ geometrix/utility/assert.hpp
@@ -9,7 +9,13 @@
 #ifndef GEOMETRIX_ASSERT_HPP
 #define GEOMETRIX_ASSERT_HPP
 
+//! Fix for used typedef in boost (which is fixed in versions beyond 1.58 I think.)
 #include <boost/static_assert.hpp>
+#if (defined(__apple_build_version__) && (__apple_build_version__ >= 7000000))
+#  define BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE __attribute__( (unused) )
+#else
+#  define BOOST_STATIC_ASSERT_UNUSED_ATTRIBUTE
+#endif
 
 #define GEOMETRIX_STATIC_ASSERT BOOST_STATIC_ASSERT
 

1bb9b25d401bb956c343959107ce035ab22b4bda
Brandon Kohn <blkohn@hotmail.com>
2015-12-29 08:41:53 -0500

    Added tests for grid and aabb intersections.

---
+++ geometry_test/eberly_triangle_aabb_intersection_tests.hpp
@@ -0,0 +1,42 @@
+//
+//! Copyright © 2008-2011
+//! Brandon Kohn
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef GEOMETRIX_EBERLY_TRIANGLE_AABB_INTERSECTION_TESTS_HPP
+#define GEOMETRIX_EBERLY_TRIANGLE_AABB_INTERSECTION_TESTS_HPP
+
+#include <boost/test/included/unit_test.hpp>
+#include <geometrix/numeric/number_comparison_policy.hpp>
+#include <geometrix/primitive/point.hpp>
+#include <geometrix/algorithm/eberly_triangle_aabb_intersection.hpp>
+
+BOOST_AUTO_TEST_CASE( TestTriangleAABBIntersection )
+{
+	using namespace geometrix;
+    typedef point<double, 2> point2;
+    absolute_tolerance_comparison_policy cmp(1e-10);
+	
+	axis_aligned_bounding_box<point2> box( point2( 0., 0. ), point2( 10., 10. ) );
+
+	//! Overlapping
+	BOOST_CHECK( eberly_triangle_aabb_intersection_2d( point2( 0, 0 ), point2( 1.0, 1.0 ), point2( 0., 1.0 ), box, cmp ) );
+
+	//! Sharing a vertex.
+	BOOST_CHECK( eberly_triangle_aabb_intersection_2d( point2( 0, 0 ), point2( -1.0, -1.0 ), point2( 0., -1.0 ), box, cmp ) );
+
+	//! disjoint
+	BOOST_CHECK( !eberly_triangle_aabb_intersection_2d( point2( -1, -1 ), point2( -4.0, -4.0 ), point2( -2., 0.0 ), box, cmp ) );
+	BOOST_CHECK( !eberly_triangle_aabb_intersection_2d( point2( 0, 10 ), point2( 10, 0 ), point2( 10, 10 ), axis_aligned_bounding_box<point2>( point2( 0., 0. ), point2( 1., 1. ) ), cmp ) );
+
+	//! Box wholly inside triangle.
+	BOOST_CHECK( eberly_triangle_aabb_intersection_2d( point2( 0, 0 ), point2( 100.0, 100.0 ), point2(80., 1.0 ), axis_aligned_bounding_box<point2>( point2( 50., 25. ), point2( 60., 35. ) ), cmp ) );
+
+	//! Triangle wholly inside box
+	BOOST_CHECK( eberly_triangle_aabb_intersection_2d( point2( 1, 1 ), point2( 2, 2 ), point2( 1.5, 2 ), axis_aligned_bounding_box<point2>( point2( 0., 0. ), point2( 10., 10. ) ), cmp ) );
+}
+
+#endif //GEOMETRIX_EBERLY_TRIANGLE_AABB_INTERSECTION_TESTS_HPP
---
+++ geometry_test/grid_tests.hpp
@@ -0,0 +1,271 @@
+//
+//! Copyright © 2008-2011
+//! Brandon Kohn
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef GEOMETRIX_GRID_TESTS_HPP
+#define GEOMETRIX_GRID_TESTS_HPP
+
+#include <boost/test/included/unit_test.hpp>
+#include <geometrix/numeric/number_comparison_policy.hpp>
+#include <geometrix/primitive/point.hpp>
+#include <geometrix/algorithm/grid_traits.hpp>
+#include <geometrix/algorithm/orientation_grid_traversal.hpp>
+#include <geometrix/algorithm/fast_voxel_grid_traversal.hpp>
+#include <geometrix/primitive/segment.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE( TestGrid )
+{
+    using namespace geometrix;
+
+    double xmin = 0;
+    double xmax = 16100;
+    double ymin = 0;
+    double ymax = 16100;
+
+    //! 10x10 miles.
+    absolute_tolerance_comparison_policy<double> cmp(1e-10);
+    grid_traits<double> grid(xmin, xmax, ymin, ymax, 2.0);
+
+    //! Test Centroid
+    {
+        BOOST_AUTO(centroid, grid.get_cell_centroid(0, 0));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(centroid, (1., 1.), 1e-10);
+    }
+    
+    {
+        BOOST_AUTO(centroid, grid.get_cell_centroid(1, 1));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(centroid, (3., 3.), 1e-10);
+    }
+
+    //! Test corner points
+    {
+        BOOST_AUTO(point0, grid.get_cell_corner0(0, 0));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(point0, (0., 0.), 1e-10);
+
+        BOOST_AUTO(point1, grid.get_cell_corner1(0, 0));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(point1, (2., 0.), 1e-10);
+
+        BOOST_AUTO(point2, grid.get_cell_corner2(0, 0));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(point2, (2., 2.), 1e-10);
+        
+        BOOST_AUTO(point3, grid.get_cell_corner3(0, 0));
+        GEOMETRIX_CHECK_SEQUENCE_CLOSE_2D(point3, (0., 2.), 1e-10);
+    }
+
+    //! memory test.
+    struct Cell { std::vector<int>* agents; std::vector<int>* obstacles; };
+    std::vector< std::vector<Cell> > data(grid.get_height(), std::vector<Cell>(grid.get_width()));//! ~8GB to build.
+    BOOST_CHECK(data.size() == grid.get_height());
+    BOOST_CHECK(data[0].size() == grid.get_width());
+}
+
+BOOST_AUTO_TEST_CASE(TestGridOrientationTraversal)
+{
+    using namespace geometrix;
+
+    double xmin = -20;
+    double xmax = 20;
+    double ymin = -20;
+    double ymax = 20;
+
+    absolute_tolerance_comparison_policy<double> cmp(1e-10);
+    grid_traits<double> grid(xmin, xmax, ymin, ymax, 1.0);
+    struct CheckingVisitor 
+    {
+        CheckingVisitor(std::vector<std::pair<std::size_t, std::size_t> >& indices)
+            : indices(indices)
+        {}
+        
+        void operator()(std::size_t i, std::size_t j)
+        {
+            indices.emplace_back(i, j);
+        }
+
+        std::vector<std::pair<std::size_t, std::size_t> >& indices; 
+    };
+
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(1., 1.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 2);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 21 && indices[1].second == 21);
+    }
+
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(.5, .5)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 1);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+    }
+
+    //! segment along +x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(2., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);        
+        BOOST_CHECK(indices[1].first == 21 && indices[1].second == 20);
+        BOOST_CHECK(indices[2].first == 22 && indices[2].second == 20);
+    }
+
+    //! reversed segment along +x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(2., 0.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 21 && indices[1].second == 20);
+        BOOST_CHECK(indices[2].first == 22 && indices[2].second == 20);
+    }
+    
+    //! segment along -x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(-2., 0.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 18 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 19 && indices[1].second == 20);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 20);
+    }
+    
+    //! segment along +y-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(0., 2.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 22);
+    }
+
+    //! reversed segment along +y-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0., 2.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 22);
+    }
+
+    //! segment in range of (45,90) degrees.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0.4, 0.5), point_double_2d(1.1, 3.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 5);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 22);
+        BOOST_CHECK(indices[3].first == 21 && indices[3].second == 22);
+        BOOST_CHECK(indices[4].first == 21 && indices[4].second == 23);
+    }
+
+    //! segment in range of (270,315) degrees.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        orientation_segment_traversal(grid, segment<point_double_2d>(point_double_2d(0.4, -0.5), point_double_2d(1.1, -3.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 4);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 19);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 18);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 17);
+        BOOST_CHECK(indices[3].first == 21 && indices[3].second == 17);
+    }
+
+    //! fast voxel method.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(1., 1.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 21 && indices[2].second == 21);
+    }
+
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(.5, .5)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 1);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+    }
+
+    //! segment along +x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(2., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 21 && indices[1].second == 20);
+        BOOST_CHECK(indices[2].first == 22 && indices[2].second == 20);
+    }
+
+    //! reversed segment along +x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(2., 0.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 20);
+        BOOST_CHECK(indices[1].first == 21 && indices[1].second == 20);
+        BOOST_CHECK(indices[0].first == 22 && indices[0].second == 20);
+    }
+
+    //! segment along -x-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(-2., 0.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 18 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 19 && indices[1].second == 20);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 20);
+    }
+
+    //! segment along +y-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0., 0.), point_double_2d(0., 2.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 22);
+    }
+
+    //! reversed segment along +y-axis.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0., 2.), point_double_2d(0., 0.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 3);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 22);
+    }
+
+    //! segment in range of (45,90) degrees.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0.4, 0.5), point_double_2d(1.1, 3.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 5);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 20);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 21);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 22);
+        BOOST_CHECK(indices[3].first == 21 && indices[3].second == 22);
+        BOOST_CHECK(indices[4].first == 21 && indices[4].second == 23);
+    }
+
+    //! segment in range of (270,315) degrees.
+    {
+        std::vector<std::pair<std::size_t, std::size_t> > indices;
+        fast_voxel_grid_traversal(grid, segment<point_double_2d>(point_double_2d(0.4, -0.5), point_double_2d(1.1, -3.)), CheckingVisitor(indices), cmp);
+        BOOST_CHECK(indices.size() == 4);
+        BOOST_CHECK(indices[0].first == 20 && indices[0].second == 19);
+        BOOST_CHECK(indices[1].first == 20 && indices[1].second == 18);
+        BOOST_CHECK(indices[2].first == 20 && indices[2].second == 17);
+        BOOST_CHECK(indices[3].first == 21 && indices[3].second == 17);
+    }
+}
+
+#endif //GEOMETRIX_GRID_TESTS_HPP

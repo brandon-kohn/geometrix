9728bc492e95605012ea6fac5938c30ce3b51b59
Brandon Kohn <blkohn@hotmail.com>
2015-12-31 18:16:43 -0500

    Added macros for condition compilation of tests.

--- geometrix/test/test.hpp
+++ geometrix/test/test.hpp
@@ -20,4 +20,31 @@
     BOOST_CHECK( geometrix::equals_within_absolute_tolerance(geometrix::get<1>(v), BOOST_PP_TUPLE_ELEM(3, 1, r), t )); \
 /***/
 
+//! Usage:
+//! This file should only be included in implementation files. Using it in headers may
+//! result in undefined behaviour if care is not made that conditional compiles occur consistently
+//! between translation units which include the header.
+//! 
+//! \code
+//! #define SOME_TEST 1 (enable SOME_TEST)
+//! ...
+//! #if GEOMETRIX_TEST_ENABLED(SOME_TEST)
+//! ...//! Do test code for case.
+//! #endif
+//! \endcode
+
+//! \def Macro to test if a conditional compilation unit (debug test) should be compiled.
+#define GEOMETRIX_TEST_ENABLED(Flag) \
+    GEOMETRIX_TESTS_ENABLED && Flag  \
+/***/
+
+//! By default debug tests are enabled. Add GEOMETRIX_DEBUG_TESTS_DISABLED to the 
+//! defines on the project to disable all such tests within the project.
+#ifndef GEOMETRIX_TESTS_ENABLED
+    #define GEOMETRIX_TESTS_ENABLED 0
+#else
+    #undef GEOMETRIX_TESTS_ENABLED
+    #define GEOMETRIX_TESTS_ENABLED 1
+#endif
+
 #endif//! GEOMETRIX_TEST_TEST_HPP

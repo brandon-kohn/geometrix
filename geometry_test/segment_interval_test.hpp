cce6df2fa1bc233db8099d2c8edce98a23c9e443
Brandon Kohn <blkohn@hotmail.com>
2016-03-01 08:58:38 -0500

    Added multiple tests for collinear_segment_difference.

--- geometry_test/segment_interval_test.hpp
+++ geometry_test/segment_interval_test.hpp
@@ -59,24 +59,166 @@ BOOST_AUTO_TEST_CASE( TestSegmentInterval )
     segment<point_2d> seg4( p7, p8 );
     segment<point_2d> seg5( p6, p5 );
 
-    std::set< segment<point_2d>, segment_interval_compare< fraction_tolerance_comparison_policy<double> > > segments;
+    std::set< segment<point_2d>, segment_interval_compare< absolute_tolerance_comparison_policy<double> > > segments;
     segments.insert( seg1 );
+	absolute_tolerance_comparison_policy<double> cmp(1e-10);
 
-    collinear_segment_difference( segments, seg2, fraction_tolerance_comparison_policy<double>( 1e-10 ) );
+    collinear_segment_difference( segments, seg2, cmp );
     print_segments( segments );   
 
-    collinear_segment_difference( segments, seg2, fraction_tolerance_comparison_policy<double>( 1e-10 ) );
+    collinear_segment_difference( segments, seg2, cmp );
     print_segments( segments );   
 
-    collinear_segment_difference( segments, seg5, fraction_tolerance_comparison_policy<double>( 1e-10 ) );
+    collinear_segment_difference( segments, seg5, cmp );
     print_segments( segments );   
 
-    collinear_segment_difference( segments, seg3, fraction_tolerance_comparison_policy<double>( 1e-10 ) );
+    collinear_segment_difference( segments, seg3, cmp );
     print_segments( segments );   
 
-    collinear_segment_difference( segments, seg4, fraction_tolerance_comparison_policy<double>( 1e-10 ) );
+    collinear_segment_difference( segments, seg4, cmp );
     print_segments( segments );   
 
+}
+
+namespace test_collinear_segment_difference_detail {
+	template <typename Segment>
+	inline bool operator ==(const Segment& s1, const Segment& s2)
+	{
+		using namespace geometrix;
+		absolute_tolerance_comparison_policy<double> cmp(1e-10);
+		return numeric_sequence_equals(get_start(s1), get_start(s2), cmp) && numeric_sequence_equals(get_end(s1), get_end(s2), cmp);
+	}
+}
+
+BOOST_AUTO_TEST_CASE(test_collinear_segment_difference)
+{
+	using namespace geometrix;
+	using namespace test_collinear_segment_difference_detail;
+	typedef point_double_2d point2;
+	typedef segment<point2> segment2;
+	typedef std::set< segment2, segment_interval_compare< absolute_tolerance_comparison_policy<double> > > clip_set;
+	absolute_tolerance_comparison_policy<double> cmp(1e-10);
+	{
+		//! A - B-C - D disjoint.
+		point2 A{ -10.0, 0.0 };
+		point2 B{ 10.0, 0.0 };
+		point2 C{ 20.0, 0.0 };
+		point2 D{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		BOOST_CHECK(segments.size() == 1 && *segments.begin() == AB);
+	}
+
+	{
+		//! A - B/C - D B==C.
+		point2 A{ -10.0, 0.0 };
+		point2 B{ 10.0, 0.0 };
+		point2 C = B;
+		point2 D{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		BOOST_CHECK(segments.size() == 1 && *segments.begin() == AB);
+	}
+
+	{
+		//! A - C-B - D overlapping.
+		point2 A{ -10.0, 0.0 };
+		point2 C{ 10.0, 0.0 };
+		point2 B{ 20.0, 0.0 };
+		point2 D{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		auto it = segments.begin();
+		BOOST_CHECK((segments.size() == 1 && *it == segment2{ A,C }));
+	}
+	
+	{
+		//! C - A-B - D overlapping.
+		point2 C{ -10.0, 0.0 };
+		point2 A{ 10.0, 0.0 };
+		point2 B{ 20.0, 0.0 };
+		point2 D{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		BOOST_CHECK((segments.empty()));
+	}
+
+	{
+		//! A - C-D - B overlapping.
+		point2 A{ -10.0, 0.0 };
+		point2 C{ 10.0, 0.0 };
+		point2 D{ 20.0, 0.0 };
+		point2 B{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		auto it = segments.begin();
+		BOOST_CHECK((segments.size() == 2 && *it == segment2{ A,C } && *++it == segment2{ D,B }));
+	}
+
+	{
+		//! A - D-C - B overlapping.
+		point2 A{ -10.0, 0.0 };
+		point2 D{ 10.0, 0.0 };
+		point2 C{ 20.0, 0.0 };
+		point2 B{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		auto it = segments.begin();
+		BOOST_CHECK((segments.size() == 2 && *it == segment2{ A, D } && *++it == segment2{ C, B }));
+	}
+
+	{
+		//! B - C-D - A overlapping.
+		point2 B{ -10.0, 0.0 };
+		point2 C{ 10.0, 0.0 };
+		point2 D{ 20.0, 0.0 };
+		point2 A{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		auto it = segments.begin();
+		BOOST_CHECK((segments.size() == 2 && *it == segment2{ C,B } && *++it == segment2{ A,D }));
+	}
+
+	{
+		//! B - C/D - C==D and overlapping.
+		point2 B{ -10.0, 0.0 };
+		point2 C{ 10.0, 0.0 };
+		point2 D = C;
+		point2 A{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		//! TODO: I do nothing in this case now, but I think there's an argument for splitting at C.
+		BOOST_CHECK((segments.size() == 1 && *segments.begin() == AB));
+	}
+
+	{
+		//! A/B-C - D disjoint.
+		point2 A{ 25.0, 0.0 };
+		point2 B = A;
+		point2 C{ 20.0, 0.0 };
+		point2 D{ 30.0, 0.0 };
+		segment2 AB{ A,B };
+		segment2 CD{ C,D };
+		clip_set segments{ AB };
+		collinear_segment_difference(segments, CD, cmp);
+		BOOST_CHECK(segments.empty());
+	}
 }
 
 #endif //GEOMETRIX_SEGMENT_INTERVAL_TESTS_HPP

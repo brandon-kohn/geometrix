957984e3ea96364d67b6ae3cb8313c4af717d96c
Brandon Kohn <blkohn@hotmail.com>
2016-03-07 08:32:08 -0500

    Added missing header.

--- geometrix/arithmetic/arithmetic.hpp
+++ geometrix/arithmetic/arithmetic.hpp
@@ -11,6 +11,7 @@
 
 #include <geometrix/numeric/numeric_traits.hpp>
 #include <geometrix/numeric/function_traits.hpp>
+#include <geometrix/utility/assert.hpp>
 
 #include <boost/typeof/typeof.hpp>
 #include <boost/utility/result_of.hpp>

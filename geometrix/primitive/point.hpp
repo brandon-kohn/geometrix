9aefd400fbdf6f25865e942fa4415ceb038a4d4b
Brandon Kohn <blkohn@hotmail.com>
2017-07-27 17:37:29 -0400

    Made homogeneous adaptors generic. Made points use generative traits. Updated tests for homogeneous_adaptor.

--- geometrix/primitive/point.hpp
+++ geometrix/primitive/point.hpp
@@ -15,6 +15,7 @@
 #include <geometrix/tensor/detail/point_generator.hpp>
 #include <geometrix/space/neutral_reference_frame.hpp>
 #include <geometrix/tensor/index_operator_vector_access_policy.hpp>
+#include <geometrix/arithmetic/arithmetic_promotion_policy.hpp>
 
 namespace geometrix {
     
@@ -30,13 +31,66 @@ typedef point<int, 3>         point_int_3d;
 
 }//namespace geometrix;
 
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_float_2d, (float), 2, float, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_float_2d> );
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_float_3d, (float), 3, float, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_float_3d> );
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_2d, (double), 2, double, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_double_2d> );
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_3d, (double), 3, double, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_double_3d> );
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_4d, (double), 4, double, double, neutral_reference_frame<4>, index_operator_vector_access_policy<point_double_4d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_float_2d, (float), 2, float, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_float_2d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_float_3d, (float), 3, float, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_float_3d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_2d, (double), 2, double, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_double_2d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_3d, (double), 3, double, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_double_3d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_double_4d, (double), 4, double, double, neutral_reference_frame<4>, index_operator_vector_access_policy<point_double_4d> );
+// 
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_int_2d, (int), 2, int, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_int_2d> );
+// GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_int_3d, (int), 3, int, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_int_3d> );
 
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_int_2d, (int), 2, int, double, neutral_reference_frame_2d, index_operator_vector_access_policy<point_int_2d> );
-GEOMETRIX_DEFINE_POINT_TRAITS( geometrix::point_int_3d, (int), 3, int, double, neutral_reference_frame_3d, index_operator_vector_access_policy<point_int_3d> );
+
+namespace boost {
+	namespace mpl {
+
+		template<typename>
+		struct sequence_tag;
+
+		template<typename T, std::size_t D>
+		struct sequence_tag<geometrix::point<T, D>>
+		{
+			typedef fusion::fusion_sequence_tag type;
+		};
+
+		template<typename T, std::size_t D>
+		struct sequence_tag<const geometrix::point<T, D>>
+		{
+			typedef fusion::fusion_sequence_tag type;
+		};
+	}
+}//! namespace boost::mpl;
+ /***/
+
+namespace geometrix {
+	template<typename T, std::size_t D>
+	struct tensor_traits< geometrix::point<T, D> >
+		: index_operator_vector_access_policy<geometrix::point<T, D>>
+	{
+		typedef index_operator_vector_access_policy<geometrix::point<T, D>> access_policy;
+		typedef boost::mpl::int_<1> tensor_order;
+		typedef void GEOMETRIX_TENSOR_ORDER(1);
+		typedef void is_tensor;
+		typedef void make_fusion_sequence;
+	};
+
+	template<typename T, std::size_t D>
+	struct geometric_traits< geometrix::point<T, D> >
+	{
+		typedef geometrix::point<T, D>                point_type;
+		typedef void                                  is_point;
+		typedef neutral_reference_frame<D>            reference_frame;
+		typedef void                                  is_coordinate_sequence;
+		typedef typename arithmetic_promotion_policy<T>::type arithmetic_type;
+		typedef decltype(std::declval<arithmetic_type>() / std::declval<arithmetic_type>()) dimensionless_type;
+		typedef void                                  is_numeric_sequence;
+		typedef geometrix::point<T, D>                sequence_type;
+		typedef GEOMETRIX_AS_MPL_VECTOR((T))          storage_types;
+		typedef dimension<D>                          dimension_type;
+		typedef void                                  is_sequence;
+		typedef void GEOMETRIX_TYPE_DIVERSITY_TAG((T));
+	};
+}//! namespace geometrix;
+ /***/
 
 #endif //GEOMETRIX_POINT_HPP
--- geometry_test/homogeneous_coordinates_tests.hpp
+++ geometry_test/homogeneous_coordinates_tests.hpp
@@ -11,26 +11,150 @@
 
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
-#include <boost/units/physical_dimensions.hpp>
-#include <boost/units/dimensionless_quantity.hpp>
-#include <boost/units/io.hpp>
-#include <boost/fusion/include/adapt_adt.hpp>
 
 #include <geometrix/tensor/vector_traits.hpp>
-#include <geometrix/tensor/homogeneous_vector_adaptor.hpp>
+#include <geometrix/tensor/homogeneous_adaptor.hpp>
 #include <geometrix/primitive/point.hpp>
 #include <geometrix/tensor/vector.hpp>
 #include <geometrix/utility/utilities.hpp>
+
+#include <geometrix/tensor/fusion_matrix.hpp>
+#include <geometrix/tensor/fusion_vector.hpp>
+
 #include "2d_kernel_units_fixture.hpp"
+
+#include <iostream>
 
 using namespace geometrix;
 
 BOOST_FIXTURE_TEST_CASE(UnitedVectorHomogeneousCoordinateAdaptorTest, geometry_kernel_2d_units_fixture)
 {
     using namespace boost::units::si;
-    auto v = vector2{ 1.0 * meters, 1.0 * meters};
+    auto v = vector2{ 1.0 * meters, 2.0 * meters};
     
-    auto hv = as_positional_homogeneous(hv);
+    auto hv = as_positional_homogeneous<length_t>(v);
+
+	BOOST_CHECK(get<0>(hv).value() == 1.0);
+	BOOST_CHECK(get<1>(hv).value() == 2.0);
+	BOOST_CHECK(get<2>(hv).value() == 1.0);
+
+	auto v2 = normalize(v);
+	auto nv = as_normalized_homogeneous<dimensionless_t>(v2);
+
+	BOOST_CHECK_CLOSE(get<0>(nv).value(), get<0>(v2).value(), 1e-10);
+	BOOST_CHECK_CLOSE(get<1>(nv).value(), get<1>(v2).value(), 1e-10);
+	BOOST_CHECK(get<2>(nv).value() == 0.0);
+}
+
+
+GEOMETRIX_FUSION_POD_MATRIX
+(
+	  xform_matrix
+	, ((double, double, geometry_kernel_2d_units_fixture::length_t))
+      ((double, double, geometry_kernel_2d_units_fixture::length_t))
+	  ((double, double, double))
+	, 3
+	, 3
+)
+
+BOOST_FIXTURE_TEST_CASE(UnitedVectorHomogeneousCoordinateAdaptorMatrixMultTest, geometry_kernel_2d_units_fixture)
+{
+	using namespace boost::units::si;
+	auto v = vector2{ 1.0 * meters, 2.0 * meters };
+
+	auto hv = as_positional_homogeneous<length_t>(v);
+
+	auto m = matrix3x3
+	{
+		1.0, 0.0, 1.0/* * meters*/
+	,	0.0, 1.0, 1.0/* * meters*/
+	,	0.0, 0.0, 1.0
+	};
+
+	auto result = m * hv;
+
+	static_assert(is_vector<decltype(result)>::value, "should return a vector");
+	BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
+	BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
+}
+
+BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorMatrixMultTest, geometry_kernel_2d_units_fixture)
+{
+	using namespace boost::units::si;
+	auto v = point2{ 1.0 * meters, 2.0 * meters };
+
+	auto hv = as_positional_homogeneous<length_t>(v);
+
+	auto m = matrix3x3
+	{
+		    1.0, 0.0, 1.0/* * meters*/
+		,	0.0, 1.0, 1.0/* * meters*/
+		,	0.0, 0.0, 1.0
+	};
+
+	auto result = m * hv;
+
+	static_assert(is_point<decltype(result)>::value, "should return a point");
+	BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
+	BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
+}
+
+BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorRotationTranslationTest, geometry_kernel_2d_units_fixture)
+{
+	using namespace boost::units::si;
+	using point3 = geometrix::point<length_t, 3>;
+	auto v = point3{ 1.0 * meters, 0.0 * meters, 0.0 * meters };
+
+	auto hv = as_positional_homogeneous<length_t>(v);
+
+	auto t = matrix4x4
+	{
+		    1.0, 0.0, 0.0, 1.0/* * meters*/
+		,	0.0, 1.0, 0.0, 1.0/* * meters*/
+		,	0.0, 0.0, 1.0, 1.0
+		,	0.0, 0.0, 0.0, 1.0
+	};
+
+	double theta = geometrix::constants::pi<double>() / 2.;
+	double sint = std::sin(theta);
+	double cost = std::cos(theta);
+	auto rz = matrix4x4
+	{
+		    cost, -sint, 0.0, 0.0
+		,   sint, cost, 0.0, 0.0
+		,	0.0, 0.0, 1.0, 0.0
+		,	0.0, 0.0, 0.0, 1.0
+	};
+
+	auto rx = matrix4x4
+	{
+		    1.0, 0.0, 0.0, 0.0
+		,   0.0, cost, -sint, 0.0
+		,	0.0, sint, cost, 0.0
+		,	0.0, 0.0, 0.0, 1.0
+	};
+
+	auto ry = matrix4x4
+	{
+		    cost, 0.0, sint, 0.0
+		,   0.0, 1.0, 0.0, 0.0
+		,	-sint, 0.0, cost, 0.0
+		,	0.0, 0.0, 0.0, 1.0
+	};
+
+	//! So this performs the following ops:
+	//! 1) Translate by vector {1,1,1}.
+	//! 2) Rotate around the x axis by theta radians.
+	//! 2) Rotate around the y axis by theta radians.
+	//! 2) Rotate around the z axis by theta radians.
+	auto result = (t * rx * ry * rz) * hv;
+
+	point2 p2 = result;
+
+	static_assert(is_point<decltype(result)>::value, "should return a point");
+
+	BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
+	BOOST_CHECK_CLOSE(get<1>(result).value(), -1.0, 1e-10);
 }
 
 #endif //GEOMETRIX_HOMOGENEOUS_COORDINATES_TESTS_HPP

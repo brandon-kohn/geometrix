751dc6202e0dc8d50ae3fe5b725ee6cf43278d74
Brandon Kohn <blkohn@hotmail.com>
2017-07-28 17:24:53 -0400

    Changed tests to avoid referring to expression past its scope.

--- geometry_test/homogeneous_coordinates_tests.hpp
+++ geometry_test/homogeneous_coordinates_tests.hpp
@@ -34,196 +34,184 @@ BOOST_FIXTURE_TEST_CASE(UnitedVectorHomogeneousCoordinateAdaptorTest, geometry_k
     
     auto hv = as_positional_homogeneous<length_t>(v);
 
-	BOOST_CHECK(get<0>(hv).value() == 1.0);
-	BOOST_CHECK(get<1>(hv).value() == 2.0);
-	BOOST_CHECK(get<2>(hv).value() == 1.0);
+    BOOST_CHECK(get<0>(hv).value() == 1.0);
+    BOOST_CHECK(get<1>(hv).value() == 2.0);
+    BOOST_CHECK(get<2>(hv).value() == 1.0);
 
-	auto v2 = normalize(v);
-	auto nv = as_normalized_homogeneous<dimensionless_t>(v2);
+    auto v2 = normalize(v);
+    auto nv = as_normalized_homogeneous<dimensionless_t>(v2);
 
-	BOOST_CHECK_CLOSE(get<0>(nv).value(), get<0>(v2).value(), 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(nv).value(), get<1>(v2).value(), 1e-10);
-	BOOST_CHECK(get<2>(nv).value() == 0.0);
+    BOOST_CHECK_CLOSE(get<0>(nv).value(), get<0>(v2).value(), 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(nv).value(), get<1>(v2).value(), 1e-10);
+    BOOST_CHECK(get<2>(nv).value() == 0.0);
 }
 
 
 GEOMETRIX_FUSION_POD_MATRIX
 (
-	  xform_matrix
-	, ((double, double, geometry_kernel_2d_units_fixture::length_t))
+      xform_matrix
+    , ((double, double, geometry_kernel_2d_units_fixture::length_t))
       ((double, double, geometry_kernel_2d_units_fixture::length_t))
-	  ((double, double, double))
-	, 3
-	, 3
+      ((double, double, double))
+    , 3
+    , 3
 )
 
 BOOST_FIXTURE_TEST_CASE(UnitedVectorHomogeneousCoordinateAdaptorMatrixMultTest, geometry_kernel_2d_units_fixture)
 {
-	using namespace boost::units::si;
-	auto v = vector2{ 1.0 * meters, 2.0 * meters };
+    using namespace boost::units::si;
+    auto v = vector2{ 1.0 * meters, 2.0 * meters };
 
-	auto hv = as_positional_homogeneous<length_t>(v);
+    auto hv = as_positional_homogeneous<length_t>(v);
 
-	auto m = matrix3x3
-	{
-		1.0, 0.0, 1.0/* * meters*/
-	,	0.0, 1.0, 1.0/* * meters*/
-	,	0.0, 0.0, 1.0
-	};
+    auto m = matrix3x3
+    {
+        1.0, 0.0, 1.0/* * meters*/
+    ,   0.0, 1.0, 1.0/* * meters*/
+    ,   0.0, 0.0, 1.0
+    };
 
-	auto result = m * hv;
+    auto result = m * hv;
 
-	static_assert(is_vector<decltype(result)>::value, "should return a vector");
-	BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
+    static_assert(is_vector<decltype(result)>::value, "should return a vector");
+    BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
 }
 
 BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorMatrixMultTest, geometry_kernel_2d_units_fixture)
 {
-	using namespace boost::units::si;
-	auto v = point2{ 1.0 * meters, 2.0 * meters };
+    using namespace boost::units::si;
+    auto v = point2{ 1.0 * meters, 2.0 * meters };
 
-	auto hv = as_positional_homogeneous<length_t>(v);
+    auto hv = as_positional_homogeneous<length_t>(v);
 
-	auto m = matrix3x3
-	{
-		    1.0, 0.0, 1.0/* * meters*/
-		,	0.0, 1.0, 1.0/* * meters*/
-		,	0.0, 0.0, 1.0
-	};
+    auto m = matrix3x3
+    {
+            1.0, 0.0, 1.0/* * meters*/
+        ,   0.0, 1.0, 1.0/* * meters*/
+        ,   0.0, 0.0, 1.0
+    };
 
-	auto result = m * hv;
+    auto result = m * hv;
 
-	static_assert(is_point<decltype(result)>::value, "should return a point");
-	BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
+    static_assert(is_point<decltype(result)>::value, "should return a point");
+    BOOST_CHECK_CLOSE(get<0>(result).value(), 2.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(result).value(), 3.0, 1e-10);
 }
 
 BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorRotationTranslation2DTest, geometry_kernel_2d_units_fixture)
 {
-	using namespace boost::units::si;
-	auto v = point2{ 1.0 * meters, 0.0 * meters };
+    using namespace boost::units::si;
+    auto v = point2{ 1.0 * meters, 0.0 * meters };
 
-	auto hv = as_positional_homogeneous<length_t>(v);
+    auto hv = as_positional_homogeneous<length_t>(v);
 
-	auto t = matrix3x3
-	{
-		    1.0, 0.0, 1.0
-		,	0.0, 1.0, 0.0
-		,	0.0, 0.0, 1.0
-	};
+    auto t = matrix3x3
+    {
+            1.0, 0.0, 1.0
+        ,   0.0, 1.0, 0.0
+        ,   0.0, 0.0, 1.0
+    };
 
-	double theta = geometrix::constants::pi<double>() / 2.;
-	double sint = std::sin(theta);
-	double cost = std::cos(theta);
-	auto rz = matrix3x3
-	{
-		    cost, -sint, 0.0
-		,   sint, cost, 0.0
-		,	0.0, 0.0, 1.0
-	};
+    double theta = geometrix::constants::pi<double>() / 2.;
+    double sint = std::sin(theta);
+    double cost = std::cos(theta);
+    auto rz = matrix3x3
+    {
+            cost, -sint, 0.0
+        ,   sint, cost, 0.0
+        ,   0.0, 0.0, 1.0
+    };
 
-	auto result = (t * rz) * hv;
+    point2 result = (t * rz) * hv;
 
-	point2 p2 = result;
-
-	static_assert(is_point<decltype(result)>::value, "should return a point");
-
-	BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
 }
 
 BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorRotationTranslation3DTest, geometry_kernel_2d_units_fixture)
 {
-	using namespace boost::units::si;
-	using point3 = geometrix::point<length_t, 3>;
-	auto v = point3{ 1.0 * meters, 0.0 * meters, 0.0 * meters };
+    using namespace boost::units::si;
+    using point3 = geometrix::point<length_t, 3>;
+    auto v = point3{ 1.0 * meters, 0.0 * meters, 0.0 * meters };
 
-	auto hv = as_positional_homogeneous<length_t>(v);
+    auto hv = as_positional_homogeneous<length_t>(v);
 
-	auto t = matrix4x4
-	{
-		    1.0, 0.0, 0.0, 1.0/* * meters*/
-		,	0.0, 1.0, 0.0, 1.0/* * meters*/
-		,	0.0, 0.0, 1.0, 1.0
-		,	0.0, 0.0, 0.0, 1.0
-	};
+    auto t = matrix4x4
+    {
+            1.0, 0.0, 0.0, 1.0/* * meters*/
+        ,   0.0, 1.0, 0.0, 1.0/* * meters*/
+        ,   0.0, 0.0, 1.0, 1.0
+        ,   0.0, 0.0, 0.0, 1.0
+    };
 
-	double theta = geometrix::constants::pi<double>() / 2.;
-	double sint = std::sin(theta);
-	double cost = std::cos(theta);
-	auto rz = matrix4x4
-	{
-		    cost, -sint, 0.0, 0.0
-		,   sint, cost, 0.0, 0.0
-		,	0.0, 0.0, 1.0, 0.0
-		,	0.0, 0.0, 0.0, 1.0
-	};
+    double theta = geometrix::constants::pi<double>() / 2.;
+    double sint = std::sin(theta);
+    double cost = std::cos(theta);
+    auto rz = matrix4x4
+    {
+            cost, -sint, 0.0, 0.0
+        ,   sint, cost, 0.0, 0.0
+        ,   0.0, 0.0, 1.0, 0.0
+        ,   0.0, 0.0, 0.0, 1.0
+    };
 
-	auto ry = matrix4x4
-	{
-		    cost, 0.0, sint, 0.0
-		,   0.0, 1.0, 0.0, 0.0
-		,	-sint, 0.0, cost, 0.0
-		,	0.0, 0.0, 0.0, 1.0
-	};
+    auto ry = matrix4x4
+    {
+            cost, 0.0, sint, 0.0
+        ,   0.0, 1.0, 0.0, 0.0
+        ,   -sint, 0.0, cost, 0.0
+        ,   0.0, 0.0, 0.0, 1.0
+    };
 
-	auto rx = matrix4x4
-	{
-		    1.0, 0.0, 0.0, 0.0
-		,   0.0, cost, -sint, 0.0
-		,	0.0, sint, cost, 0.0
-		,	0.0, 0.0, 0.0, 1.0
-	};
+    auto rx = matrix4x4
+    {
+            1.0, 0.0, 0.0, 0.0
+        ,   0.0, cost, -sint, 0.0
+        ,   0.0, sint, cost, 0.0
+        ,   0.0, 0.0, 0.0, 1.0
+    };
 
-	//! So this performs the following ops:
-	//! 1) Translate by vector {1,1,1}.
-	//! 2) Rotate around the z axis by theta radians.
-	//! 2) Rotate around the y axis by theta radians.
-	//! 2) Rotate around the x axis by theta radians.
-	auto result = (t * rz * ry * rx) * hv;
+    //! So this performs the following ops:
+    //! 1) Translate by vector {1,1,1}.
+    //! 2) Rotate around the z axis by theta radians.
+    //! 2) Rotate around the y axis by theta radians.
+    //! 2) Rotate around the x axis by theta radians.
+    point2 result = (t * rz * ry * rx) * hv;
 
-	point2 p2 = result;
-
-	static_assert(is_point<decltype(result)>::value, "should return a point");
-
-	BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
 }
 
 
 BOOST_FIXTURE_TEST_CASE(PointHomogeneousCoordinateAdaptorRotationTranslation2DDTest, geometry_kernel_2d_units_fixture)
 {
-	using namespace boost::units::si;
-	auto v = point2{ 1.0 * meters, 0.0 * meters };
+    using namespace boost::units::si;
+    auto v = point2{ 1.0 * meters, 0.0 * meters };
 
-	auto hv = as_positional_homogeneous<length_t>(v);
+    auto hv = as_positional_homogeneous<length_t>(v);
 
-	auto t = matrix3x3
-	{
-	    	1.0, 0.0, 1.0
-		,	0.0, 1.0, 0.0
-		,	0.0, 0.0, 1.0
-	};
+    auto t = matrix3x3
+    {
+            1.0, 0.0, 1.0
+        ,   0.0, 1.0, 0.0
+        ,   0.0, 0.0, 1.0
+    };
 
-	double theta = geometrix::constants::pi<double>() / 2.;
-	double sint = std::sin(theta);
-	double cost = std::cos(theta);
-	auto rz = matrix3x3
-	{
-		    cost, -sint, 0.0
-		,   sint, cost, 0.0
-		,	0.0, 0.0, 1.0
-	};
+    double theta = geometrix::constants::pi<double>() / 2.;
+    double sint = std::sin(theta);
+    double cost = std::cos(theta);
+    auto rz = matrix3x3
+    {
+            cost, -sint, 0.0
+        ,   sint, cost, 0.0
+        ,   0.0, 0.0, 1.0
+    };
 
-	auto result = (t * rz) * hv;
+    point2 result = (t * rz) * hv;
 
-	point2 p2 = result;
-
-	static_assert(is_point<decltype(result)>::value, "should return a point");
-
-	BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
-	BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<0>(result).value(), 1.0, 1e-10);
+    BOOST_CHECK_CLOSE(get<1>(result).value(), 1.0, 1e-10);
 }
 
 #endif //GEOMETRIX_HOMOGENEOUS_COORDINATES_TESTS_HPP

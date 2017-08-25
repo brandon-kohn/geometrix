8f2547e2bb5a2547b3ef372891e58b101225c8d0
Brandon Kohn <blkohn@hotmail.com>
2017-08-25 17:03:21 -0400

    Added natvis for visual studio. Added construction traits for type double from boost units. Modified GEOMETRIX_DEFINE_NUMERIC_TRAITS macro to define default construction policy which uses boost::numeric_cast.

--- geometrix/numeric/boost_units_quantity.hpp
+++ geometrix/numeric/boost_units_quantity.hpp
@@ -19,49 +19,59 @@
 
 namespace geometrix {
 
-	//! Define numeric traits for the coordinate type.
-	template <typename Unit, typename Numeric>
-	struct is_numeric< boost::units::quantity< Unit, Numeric > > : boost::true_type {};
+    //! Define numeric traits for the coordinate type.
+    template <typename Unit, typename Numeric>
+    struct is_numeric< boost::units::quantity< Unit, Numeric > > : boost::true_type {};
 
-	template <typename Unit, typename Numeric>
-	struct numeric_traits< boost::units::quantity< Unit, Numeric > >
-	{
-		typedef boost::units::quantity< Unit, Numeric >      numeric_type;
-		typedef typename boost::is_float< Numeric >::type    is_float;
-		typedef typename boost::is_integral< Numeric >::type is_integral;
-		typedef void                                         is_numeric;
-		static const unsigned int digits = std::numeric_limits<Numeric>::digits10;
+    template <typename Unit, typename Numeric>
+    struct numeric_traits< boost::units::quantity< Unit, Numeric > >
+    {
+        typedef boost::units::quantity< Unit, Numeric >      numeric_type;
+        typedef typename boost::is_float< Numeric >::type    is_float;
+        typedef typename boost::is_integral< Numeric >::type is_integral;
+        typedef void                                         is_numeric;
+        static const unsigned int digits = std::numeric_limits<Numeric>::digits10;
 
-		BOOST_MPL_ASSERT_MSG
-		(
-			(!(is_float::value && is_integral::value))
-			, NUMERIC_TRAITS_CANNOT_DEFINE_FLOAT_AND_INTEGRAL
-			, (boost::units::quantity< Unit, Numeric >)
-		);
-	};
+        BOOST_MPL_ASSERT_MSG
+        (
+            (!(is_float::value && is_integral::value))
+            , NUMERIC_TRAITS_CANNOT_DEFINE_FLOAT_AND_INTEGRAL
+            , (boost::units::quantity< Unit, Numeric >)
+        );
+    };
 
-	template <typename Unit, typename T>
-	struct is_dimensionless<boost::units::quantity<Unit, T>>
-		: boost::units::is_dimensionless_unit<Unit>
-	{};
+    template <typename Unit, typename T>
+    struct is_dimensionless<boost::units::quantity<Unit, T>>
+        : boost::units::is_dimensionless_unit<Unit>
+    {};
 
-	template <typename Unit, typename Numeric>
-	struct construction_policy< boost::units::quantity< Unit, Numeric > >
-	{
-		template <typename T>
-		static boost::units::quantity< Unit, Numeric > construct(T&& n) { return boost::units::quantity< Unit, Numeric >(boost::numeric_cast<Numeric>(geometrix::get(n)) * Unit()); }
-		template <typename T, typename Unit2>
-		static boost::units::quantity< Unit, Numeric > construct(const boost::units::quantity<Unit2, T>& n) { return boost::units::quantity< Unit, Numeric >(n); }
-	};
+    template <typename Unit, typename Numeric>
+    struct construction_policy< boost::units::quantity< Unit, Numeric > >
+    {
+        template <typename T>
+        static boost::units::quantity< Unit, Numeric > construct(T&& n) { return boost::units::quantity< Unit, Numeric >(boost::numeric_cast<Numeric>(geometrix::get(n)) * Unit()); }
+        template <typename T, typename Unit2>
+        static boost::units::quantity< Unit, Numeric > construct(const boost::units::quantity<Unit2, T>& n) { return boost::units::quantity< Unit, Numeric >(n); }
+    };
 
-	template <typename Unit, typename Numeric>
-	struct construction_policy< const boost::units::quantity< Unit, Numeric > >
-	{
-		template <typename T>
-		static const boost::units::quantity< Unit, Numeric > construct(T&& n) { return boost::units::quantity< Unit, Numeric >(boost::numeric_cast<Numeric>(geometrix::get(n)) * Unit()); }
-		template <typename T, typename Unit2>
-		static const boost::units::quantity< Unit, Numeric > construct(const boost::units::quantity<Unit2, T>& n) { return boost::units::quantity< Unit, Numeric >(n); }
-	};
+    template <typename Unit, typename Numeric>
+    struct construction_policy< const boost::units::quantity< Unit, Numeric > >
+    {
+        template <typename T>
+        static const boost::units::quantity< Unit, Numeric > construct(T&& n) { return boost::units::quantity< Unit, Numeric >(boost::numeric_cast<Numeric>(geometrix::get(n)) * Unit()); }
+        template <typename T, typename Unit2>
+        static const boost::units::quantity< Unit, Numeric > construct(const boost::units::quantity<Unit2, T>& n) { return boost::units::quantity< Unit, Numeric >(n); }
+    };
+
+    template<>
+    struct construction_policy< double >
+    {
+        template <typename T>
+        static double construct(T&& n) { return boost::numeric_cast<double>(geometrix::get(n)); }
+
+        template <typename T, typename Unit>
+        static double construct(const boost::units::quantity<Unit, T>& n) { return boost::numeric_cast<double>(n.value()); }
+    };
 
 }//namespace geometrix;
 
--- geometrix/numeric/numeric_traits.hpp
+++ geometrix/numeric/numeric_traits.hpp
@@ -15,10 +15,11 @@
 #include <boost/mpl/and.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/limits.hpp>
+#include <boost/numeric/conversion/cast.hpp>
 
 namespace geometrix {
-    
-//! Default numeric traits struct. 
+
+//! Default numeric traits struct.
 //! NOTE: must be specialized for user types.
 template <typename NumericType, typename EnableIf=void>
 struct numeric_traits
@@ -32,7 +33,7 @@ template <typename NumericType>
 struct is_numeric<NumericType, typename numeric_traits<typename geometrix::remove_const_ref<NumericType>::type>::is_numeric> : boost::true_type{};
 
 template <typename T1, typename T2, typename EnableIf=void>
-struct numeric_width_compare 
+struct numeric_width_compare
     : boost::mpl::less
         <
             boost::mpl::int_<numeric_traits< typename geometrix::remove_const_ref<T1>::type >::digits>
@@ -42,58 +43,58 @@ struct numeric_width_compare
 
 template <typename T1, typename T2>
 struct numeric_width_compare
-    < 
-        T1
-      , T2
-      , typename boost::enable_if
-        < 
-            boost::mpl::and_
-            < 
-                typename numeric_traits<typename geometrix::remove_const_ref<T1>::type>::is_integral
-              , typename numeric_traits<typename geometrix::remove_const_ref<T2>::type>::is_float 
-            > 
-        >::type 
-    > : boost::true_type {};
-
-template <typename T1, typename T2>
-struct numeric_width_compare
-    < 
+    <
         T1
       , T2
       , typename boost::enable_if
         <
             boost::mpl::and_
-            < 
+            <
+                typename numeric_traits<typename geometrix::remove_const_ref<T1>::type>::is_integral
+              , typename numeric_traits<typename geometrix::remove_const_ref<T2>::type>::is_float
+            >
+        >::type
+    > : boost::true_type {};
+
+template <typename T1, typename T2>
+struct numeric_width_compare
+    <
+        T1
+      , T2
+      , typename boost::enable_if
+        <
+            boost::mpl::and_
+            <
                 typename numeric_traits<typename geometrix::remove_const_ref<T1>::type>::is_float
-              , typename numeric_traits<typename geometrix::remove_const_ref<T2>::type>::is_integral 
-            > 
+              , typename numeric_traits<typename geometrix::remove_const_ref<T2>::type>::is_integral
+            >
         >::type
     > : boost::false_type {};
 
-template <typename NumericType>                                                                           
-struct numeric_traits< NumericType, typename boost::enable_if< boost::is_arithmetic<NumericType> >::type >                                                  
-{                                                                                     
-    typedef NumericType                                       numeric_type;                    
-    typedef typename boost::is_float< numeric_type >::type    is_float;                        
-    typedef typename boost::is_integral< numeric_type >::type is_integral;                     
+template <typename NumericType>
+struct numeric_traits< NumericType, typename boost::enable_if< boost::is_arithmetic<NumericType> >::type >
+{
+    typedef NumericType                                       numeric_type;
+    typedef typename boost::is_float< numeric_type >::type    is_float;
+    typedef typename boost::is_integral< numeric_type >::type is_integral;
     typedef void                                              is_numeric;
 
-    BOOST_STATIC_CONSTANT                                                             
-    (                                                                                 
-        unsigned int                                                                  
-      , digits = std::numeric_limits<numeric_type>::digits10                          
-    );                                                                                
-                                                                                      
-    BOOST_MPL_ASSERT_MSG(                                                             
-        ( !( is_float::value && is_integral::value ) )                                
-        , NUMERIC_TRAITS_CANNOT_DEFINE_FLOAT_AND_INTEGRAL
-        , (NumericType) );                                                            
-};                                                                                    
+    BOOST_STATIC_CONSTANT
+    (
+        unsigned int
+      , digits = std::numeric_limits<numeric_type>::digits10
+    );
 
-template <typename NumericType>                                                                           
-struct construction_policy< NumericType, typename boost::enable_if< boost::is_arithmetic<NumericType> >::type  >                                             
+    BOOST_MPL_ASSERT_MSG(
+        ( !( is_float::value && is_integral::value ) )
+        , NUMERIC_TRAITS_CANNOT_DEFINE_FLOAT_AND_INTEGRAL
+        , (NumericType) );
+};
+
+template <typename NumericType>
+struct construction_policy< NumericType, typename boost::enable_if< boost::is_arithmetic<NumericType> >::type  >
 {
-    static const NumericType& construct( const NumericType& t ){ return t; } 
+    static const NumericType& construct( const NumericType& t ){ return t; }
 };
 
 template <typename NumericType, typename EnableIf = void>
@@ -123,7 +124,14 @@ struct numeric_traits< NumericType >                                            
 };                                                                                    \
 template <>                                                                           \
 struct construction_policy< NumericType >                                             \
-{ static const NumericType& construct( const NumericType& t ){ return t; } };         \
+{                                                                                     \
+    static const NumericType& construct( const NumericType& t ){ return t; } };       \
+    template <typename V>                                                             \
+    static const NumericType& construct( V&& t )                                      \
+    {                                                                                 \
+        return boost::numeric_cast<NumericType>(geometrix::get(v));                   \
+    }                                                                                 \
+};                                                                                    \
 }                                                                                     \
 /***/
 

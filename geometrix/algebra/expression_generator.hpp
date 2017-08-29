9da20f9a1fc70b2af872c70308f5f370012368b9
Brandon Kohn <blkohn@hotmail.com>
2017-08-29 10:47:02 -0400

    Cleaned up whitespace.

--- geometrix/algebra/expression_generator.hpp
+++ geometrix/algebra/expression_generator.hpp
@@ -147,7 +147,7 @@ struct expr
     >
 {
     struct traits : boost::result_of<geometrix::grammar(Expr)>::type {};
-    GEOMETRIX_PROTO_BASIC_EXTENDS(Expr, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, domain);    
+    GEOMETRIX_PROTO_BASIC_EXTENDS(Expr, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, domain);
 };
 
 template<typename Expr, typename EnableIf=void>
@@ -169,7 +169,7 @@ struct expr_access_policy
     >
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> scalar_expression;
-    
+
     struct type_at
         : boost::proto::result_of::eval
           <
@@ -177,8 +177,8 @@ struct expr_access_policy
             , typename scalar_expression::traits::context
           >
     {};
-        
-    static typename type_at::type get( const scalar_expression& c ) 
+
+    static typename type_at::type get( const scalar_expression& c )
     {
         return boost::proto::eval( c, typename scalar_expression::traits::context() );
     }
@@ -186,21 +186,21 @@ struct expr_access_policy
 
 template <typename Expr>
 struct expr_access_policy
-    < 
+    <
         Expr
       , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_sequence
     >
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> sequence_expression;
-    
-	template <std::size_t I>
+
+    template <std::size_t I>
     struct type_at
         : boost::proto::result_of::eval
             <
                 sequence_expression
               , typename sequence_expression::traits::template context<I>
             >
-    {   
+    {
         BOOST_MPL_ASSERT_MSG
         (
             ( I < sequence_expression::traits::dimension_type::value )
@@ -208,10 +208,10 @@ struct expr_access_policy
             , (sequence_expression)
         );
     };
-    
+
     //! \brief compile time access
     template <std::size_t Index>
-    static typename type_at<Index>::type get( const sequence_expression& v ) 
+    static typename type_at<Index>::type get( const sequence_expression& v )
     {
         return boost::proto::eval( v, typename sequence_expression::traits::template context<Index>() );
     }
@@ -225,7 +225,7 @@ struct expr_access_policy
     >
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> matrix_expression;
-    
+
     template <std::size_t Row, std::size_t Column>
     struct type_at
         : boost::proto::result_of::eval
@@ -233,7 +233,7 @@ struct expr_access_policy
                 matrix_expression
               , typename matrix_expression::traits::template context<Row, Column>
             >
-    {        
+    {
         static_assert
         (
             Row < matrix_expression::traits::row_dimension::type::value &&
@@ -241,10 +241,10 @@ struct expr_access_policy
             , "matrix indices out of bounds"
         );
     };
-   
+
     //! \brief compile time access
     template <std::size_t Row, std::size_t Column>
-    static typename type_at<Row, Column>::type get( const matrix_expression& m ) 
+    static typename type_at<Row, Column>::type get( const matrix_expression& m )
     {
         return boost::proto::eval( m, typename matrix_expression::traits::template context<Row, Column>() );
     }
@@ -254,13 +254,13 @@ namespace detail
 {
     template <typename T, typename Expr, typename TagLHS=void, typename TagRHS=void>
     struct assigner
-    {    
+    {
         BOOST_MPL_ASSERT_MSG
         (
                 (false)
               , CALL_TO_ASSIGN_LINEAR_ALGEBRA_EXPRESSION_RESULT_HAS_INCOMPATIBLE_TYPE
               , (T)
-        );      
+        );
     };
 
     template <typename T, typename Expr>
@@ -270,10 +270,10 @@ namespace detail
           , Expr
           , typename geometrix::geometric_traits<T>::is_scalar
           , typename geometrix::geometric_traits<Expr>::is_scalar
-        > 
+        >
     {
         template <typename LHS, typename RHS>
-        static void assign( LHS& l, const RHS& e ) 
+        static void assign( LHS& l, const RHS& e )
         {
             l = geometrix::get(e);
         }
@@ -286,10 +286,10 @@ namespace detail
           , Expr
           , typename geometrix::geometric_traits<T>::is_vector
           , typename geometrix::geometric_traits<Expr>::is_vector
-        > 
+        >
     {
         template <typename LHS, typename RHS>
-        static void assign( LHS& l, const RHS& e ) 
+        static void assign( LHS& l, const RHS& e )
         {
             l = geometrix::construct<T>(e);
         }
@@ -302,10 +302,10 @@ namespace detail
           , Expr
           , typename geometrix::geometric_traits<T>::is_point
           , typename geometrix::geometric_traits<Expr>::is_point
-        > 
+        >
     {
         template <typename LHS, typename RHS>
-        static void assign( LHS& l, const RHS& e ) 
+        static void assign( LHS& l, const RHS& e )
         {
             l = geometrix::construct<T>(e);
         }
@@ -318,10 +318,10 @@ namespace detail
           , Expr
           , typename geometrix::geometric_traits<T>::is_matrix
           , typename geometrix::geometric_traits<Expr>::is_matrix
-        > 
+        >
     {
         template <typename LHS, typename RHS>
-        static void assign( LHS& l, const RHS& e ) 
+        static void assign( LHS& l, const RHS& e )
         {
             l = geometrix::construct<LHS>(e);
         }
@@ -333,7 +333,7 @@ template <typename LHS, typename Expr>
 inline LHS& operator <<= ( LHS& lhs, const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& rhs )
 {
     detail::assigner<LHS, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> >::template assign( lhs, rhs );
-	return lhs;
+    return lhs;
 }
 
 //! Assign from the same type.
@@ -358,67 +358,67 @@ struct expr_cast_wrapper
 template <typename T, typename Expr>
 inline expr_cast_wrapper<T,Expr> expr_cast( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e )
 {
-    return expr_cast_wrapper<T,Expr>( e ); 
+    return expr_cast_wrapper<T,Expr>( e );
 }
 
 template <typename LHS, typename T, typename Expr>
 inline LHS& operator <<= ( LHS& lhs, const expr_cast_wrapper<T, Expr>& rhs )
 {
     detail::assigner<LHS, T>::template assign( lhs, rhs.e );
-	return lhs;
+    return lhs;
 }
 
 GEOMETRIX_EXPRESSION_NAMESPACE_END
 
-namespace geometrix { 
+namespace geometrix {
 
 template <typename T, typename Expr>
 struct as_converter < T, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_scalar >
 {
-	T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
-	{
-		return get( e );
-	}
+    T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
+    {
+        return get( e );
+    }
 };
 
 template <typename T, typename Expr>
 struct as_converter < T, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_vector  >
 {
-	T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
-	{
-		return construct<T>( e );
-	}
+    T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
+    {
+        return construct<T>( e );
+    }
 };
 
 template <typename T, typename Expr>
 struct as_converter < T, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_point >
 {
-	T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
-	{
-		return construct<T>( e );
-	}
+    T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
+    {
+        return construct<T>( e );
+    }
 };
 
 template <typename T, typename Expr>
 struct as_converter < T, GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_matrix >
 {
-	T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
-	{
-		return construct<T>( e );
-	}
+    T operator()( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& e ) const
+    {
+        return construct<T>( e );
+    }
 };
 
 template <typename Expr>
 struct tensor_traits
     <
         GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr< Expr >
-	  , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_scalar
+      , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_scalar
     >
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr_access_policy< Expr > access_policy;
-    typedef void                                is_tensor;    
-	typedef boost::mpl::int_<0>                 tensor_order;
+    typedef void                                is_tensor;
+    typedef boost::mpl::int_<0>                 tensor_order;
     typedef void                                make_fusion_sequence;//Generate the fusion adaptor for the accesses to this.
 };
 
@@ -426,13 +426,13 @@ template <typename Expr>
 struct tensor_traits
     <
         const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr< Expr >
-	  , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_scalar
+      , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_scalar
     >
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr_access_policy< Expr > access_policy;
-    typedef void                                is_tensor;    
-	typedef boost::mpl::int_<0>                 tensor_order;
+    typedef void                                is_tensor;
+    typedef boost::mpl::int_<0>                 tensor_order;
     typedef void                                make_fusion_sequence;//Generate the fusion adaptor for the accesses to this.
 };
 
@@ -440,13 +440,13 @@ template <typename Expr>
 struct tensor_traits
     <
         GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr< Expr >
-	  , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_vector
+      , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_vector
     >
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr_access_policy< Expr > access_policy;
-    typedef void                                is_tensor;    
-	typedef boost::mpl::int_<1>                 tensor_order;
+    typedef void                                is_tensor;
+    typedef boost::mpl::int_<1>                 tensor_order;
     typedef void                                make_fusion_sequence;//Generate the fusion adaptor for the accesses to this.
 };
 
@@ -454,13 +454,13 @@ template <typename Expr>
 struct tensor_traits
     <
         GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr< Expr >
-	  , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_point
+      , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_point
     >
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr_access_policy< Expr > access_policy;
-    typedef void                                is_tensor;    
-	typedef boost::mpl::int_<1>                 tensor_order;
+    typedef void                                is_tensor;
+    typedef boost::mpl::int_<1>                 tensor_order;
     typedef void                                make_fusion_sequence;//Generate the fusion adaptor for the accesses to this.
 };
 
@@ -468,13 +468,13 @@ template <typename Expr>
 struct tensor_traits
     <
         GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr< Expr >
-	  , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_matrix
+      , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits::is_matrix
     >
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>::traits
 {
     typedef GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr_access_policy< Expr > access_policy;
-    typedef void                                is_tensor;    
-	typedef boost::mpl::int_<2>                 tensor_order;
+    typedef void                                is_tensor;
+    typedef boost::mpl::int_<2>                 tensor_order;
     typedef void                                make_fusion_sequence;//Generate the fusion adaptor for the accesses to this.
 };
 
@@ -494,13 +494,13 @@ struct is_scalar
     > : boost::true_type
 {};
 
-template <typename T> 
+template <typename T>
 struct numeric_traits
     <
         GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T>
       , typename GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T>::traits::is_scalar
     >
-{                                                                                     
+{
     typedef typename boost::proto::result_of::eval
         <
             T
@@ -509,28 +509,28 @@ struct numeric_traits
     typedef typename boost::is_float< numeric_type >::type    is_float;
     typedef typename boost::is_integral< numeric_type >::type is_integral;
     static const numeric_type                                 zero(){ return numeric_traits<numeric_type>::zero(); }
-    static const numeric_type                                 one(){ return numeric_traits<numeric_type>::one(); } 
-    BOOST_STATIC_CONSTANT                                                             
-    (                                                                                 
+    static const numeric_type                                 one(){ return numeric_traits<numeric_type>::one(); }
+    BOOST_STATIC_CONSTANT
+    (
         unsigned int
       , digits = std::numeric_limits<numeric_type>::digits10
     );
-};          
+};
 
 template <typename T>
-struct geometric_traits<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T>> 
+struct geometric_traits<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T>>
     : GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T>::traits
 {};
 
 //! Row/Column
-template <typename Expr, std::size_t Column> 
+template <typename Expr, std::size_t Column>
 struct column< GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, Column>
 {
     typedef boost::mpl::int_<Column> index;
-    column( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& v )                                                                                      
-        : m(v)                                                                                       
-    {};         
-    
+    column( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& v )
+        : m(v)
+    {};
+
     template <std::size_t Index>
     struct type_at
     {
@@ -538,56 +538,56 @@ struct column< GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, Column>
             <
                 GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>
               , Index
-              , Column 
+              , Column
             >::type type;
     };
 
-    template <std::size_t Row>                                                                                                       
-    typename type_at<Row>::type get() const  
-    {                                                                                                             
+    template <std::size_t Row>
+    typename type_at<Row>::type get() const
+    {
         return geometrix::get<Row, Column>( m );
-    }                                                                                                                                 
-    
+    }
+
     const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& m;
 };
 
 template <typename Expr, std::size_t Column>
-struct geometric_traits< column<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, Column> > 
+struct geometric_traits< column<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, Column> >
     : diversity_base< GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> >
 {
     typedef typename row_dimension_of
-        < 
-            GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> 
+        <
+            GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>
         >::type  dimension_type;
     typedef void is_sequence;
-    typedef void is_numeric_sequence;    
+    typedef void is_numeric_sequence;
 };
 
-template <typename Expr, std::size_t Row> 
+template <typename Expr, std::size_t Row>
 struct row< GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>, Row>
 {
     typedef boost::mpl::int_<Row> index;
-    row( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& v )                                                                                      
-        : m(v)                                       
+    row( const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& v )
+        : m(v)
     {};
-    
+
     template <std::size_t Index>
     struct type_at
     {
         typedef typename geometrix::type_at
-            < 
+            <
                 GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>
               , Row
-              , Index 
+              , Index
             >::type type;
     };
 
     template <std::size_t Column>
-    typename type_at<Column>::type get() const  
-    {                                                                                                             
+    typename type_at<Column>::type get() const
+    {
         return GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::get<Row, Column>( m );
-    }                                                                                                                                 
-    
+    }
+
     const GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>& m;
 };
 
@@ -597,7 +597,7 @@ struct geometric_traits< row<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>,In
 {
     typedef typename column_dimension_of
         <
-            GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr> 
+            GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>
         >::type  dimension_type;
     typedef void is_sequence;
     typedef void is_numeric_sequence;
@@ -608,17 +608,17 @@ struct geometric_traits< row<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<Expr>,In
 //! Mark the expr as an MPL type sequence.
 namespace boost { namespace mpl {
 
-template<typename>                              
-struct sequence_tag;                            
-                                                
-template<typename T>                            
+template<typename>
+struct sequence_tag;
+
+template<typename T>
 struct sequence_tag< GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T> >
-{                                               
-    typedef fusion::fusion_sequence_tag type;   
-};                                              
-template<typename T>             
-struct sequence_tag<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T> const>                    
-{                                               
-    typedef fusion::fusion_sequence_tag type;   
-};                                              
+{
+    typedef fusion::fusion_sequence_tag type;
+};
+template<typename T>
+struct sequence_tag<GEOMETRIX_EXPRESSION_NAMESPACE_SCOPE::expr<T> const>
+{
+    typedef fusion::fusion_sequence_tag type;
+};
 }}//namespace boost::mpl

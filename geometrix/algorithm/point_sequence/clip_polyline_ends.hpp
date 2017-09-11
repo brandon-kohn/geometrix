08e0c6c33cbd8c1cc98e3de3375590649a656dca
Brandon Kohn <blkohn@hotmail.com>
2017-09-11 10:18:23 -0400

    Whitespace changes.

--- geometrix/algorithm/point_sequence/clip_polyline_ends.hpp
+++ geometrix/algorithm/point_sequence/clip_polyline_ends.hpp
@@ -20,147 +20,147 @@
 
 namespace geometrix {
 
-	template <typename Polyline>
-	inline std::vector<typename geometric_traits<typename point_sequence_traits<Polyline>::point_type>::arithmetic_type> polyline_segment_lengths(const Polyline& poly)
-	{
-		typedef point_sequence_traits<Polyline> access;
-		typedef typename point_sequence_traits<Polyline>::point_type point_type;
-		typedef typename geometric_traits<point_type>::arithmetic_type arithmetic_type;
-		arithmetic_type length = 0;
-		std::size_t size = access::size(poly);
-		std::vector<arithmetic_type> lengths;
-		
-		for (std::size_t i = 0, j = 1; j < size; i = j++) 
-		{
-			length += point_point_distance(access::get_point(poly, i), access::get_point(poly, j));
-			lengths.push_back(length);
-		}
+    template <typename Polyline>
+    inline std::vector<typename geometric_traits<typename point_sequence_traits<Polyline>::point_type>::arithmetic_type> polyline_segment_lengths(const Polyline& poly)
+    {
+        typedef point_sequence_traits<Polyline> access;
+        typedef typename point_sequence_traits<Polyline>::point_type point_type;
+        typedef typename geometric_traits<point_type>::arithmetic_type arithmetic_type;
+        arithmetic_type length = 0;
+        std::size_t size = access::size(poly);
+        std::vector<arithmetic_type> lengths;
 
-		return std::move(lengths);
-	}
+        for (std::size_t i = 0, j = 1; j < size; i = j++)
+        {
+            length += point_point_distance(access::get_point(poly, i), access::get_point(poly, j));
+            lengths.push_back(length);
+        }
 
-	template <typename PolylineIter, typename ArithmeticType>
-	inline std::vector<ArithmeticType> polyline_segment_lengths(PolylineIter first, PolylineIter last, const ArithmeticType& cutoff)
-	{	
-		ArithmeticType length = 0;
-		std::vector<ArithmeticType> lengths;
-		
-		if (first == last)
-			return std::move(lengths);
+        return std::move(lengths);
+    }
 
-		for (PolylineIter i = first, j = std::next(first); j != last; ++i, ++j)
-		{
-			length += point_point_distance(*i, *j);
-			lengths.push_back(length);
-			if (lengths.back() > cutoff)
-				break;
-		}
+    template <typename PolylineIter, typename ArithmeticType>
+    inline std::vector<ArithmeticType> polyline_segment_lengths(PolylineIter first, PolylineIter last, const ArithmeticType& cutoff)
+    {
+        ArithmeticType length = 0;
+        std::vector<ArithmeticType> lengths;
 
-		return std::move(lengths);
-	}
+        if (first == last)
+            return std::move(lengths);
 
-	template <typename Polyline, typename ArithmeticType>
-	inline std::vector<typename geometric_traits<typename point_sequence_traits<Polyline>::point_type>::arithmetic_type> polyline_segment_lengths(const Polyline& poly, const ArithmeticType& cutoff)
-	{
-		typedef point_sequence_traits<Polyline> access;
-		typedef typename point_sequence_traits<Polyline>::point_type point_type;
-		typedef typename geometric_traits<point_type>::arithmetic_type arithmetic_type;
-		arithmetic_type length = 0;
-		std::size_t size = access::size(poly);		
-		std::vector<arithmetic_type> lengths;
-		
-		for (std::size_t i = 0, j = 1; j < size; i = j++)
-		{
-			length += point_point_distance(access::get_point(poly, i), access::get_point(poly, j));
-			lengths.push_back(length);
-			if (lengths.back() > cutoff)
-				break;
-		}
+        for (PolylineIter i = first, j = std::next(first); j != last; ++i, ++j)
+        {
+            length += point_point_distance(*i, *j);
+            lengths.push_back(length);
+            if (lengths.back() > cutoff)
+                break;
+        }
 
-		return std::move(lengths);
-	}
-	
-	template <typename Polyline, typename PolylineIter, typename ArithmeticType, typename NumberComparisonPolicy>
-	inline Polyline clip_polyline_front(PolylineIter first, PolylineIter last, ArithmeticType l, const NumberComparisonPolicy& cmp)
-	{
-		typedef typename point_sequence_traits<Polyline>::point_type point_type;
-		
-		std::vector<ArithmeticType> lengths = polyline_segment_lengths(first, last, l);
-		auto it = std::lower_bound(lengths.begin(), lengths.end(), l);
+        return std::move(lengths);
+    }
 
-		Polyline newPoly;
-		if (it == lengths.end())
-			return std::move(newPoly);
-				
-		l = *it - l;
+    template <typename Polyline, typename ArithmeticType>
+    inline std::vector<typename geometric_traits<typename point_sequence_traits<Polyline>::point_type>::arithmetic_type> polyline_segment_lengths(const Polyline& poly, const ArithmeticType& cutoff)
+    {
+        typedef point_sequence_traits<Polyline> access;
+        typedef typename point_sequence_traits<Polyline>::point_type point_type;
+        typedef typename geometric_traits<point_type>::arithmetic_type arithmetic_type;
+        arithmetic_type length = 0;
+        std::size_t size = access::size(poly);
+        std::vector<arithmetic_type> lengths;
 
-		auto index = std::distance(lengths.begin(), it);
-		PolylineIter pit = std::next(first, index), pnext = std::next(first, index + 1);
-		GEOMETRIX_ASSERT(pnext != last);
+        for (std::size_t i = 0, j = 1; j < size; i = j++)
+        {
+            length += point_point_distance(access::get_point(poly, i), access::get_point(poly, j));
+            lengths.push_back(length);
+            if (lengths.back() > cutoff)
+                break;
+        }
 
-		if (cmp.greater_than(l, constants::zero<ArithmeticType>()))
-		{
-			point_type newP{ *pnext + l * normalize(*pit - *pnext) };
-			if (!numeric_sequence_equals(newP, *pnext, cmp))
-				newPoly.emplace_back(newP);
-		}
-		else if (++pnext != last)
-			newPoly.push_back(*std::prev(pnext));
+        return std::move(lengths);
+    }
 
-		for (; pnext != last; ++pnext)
-			newPoly.push_back(*pnext);
+    template <typename Polyline, typename PolylineIter, typename ArithmeticType, typename NumberComparisonPolicy>
+    inline Polyline clip_polyline_front(PolylineIter first, PolylineIter last, ArithmeticType l, const NumberComparisonPolicy& cmp)
+    {
+        typedef typename point_sequence_traits<Polyline>::point_type point_type;
 
-		return std::move(newPoly);
-	}
+        std::vector<ArithmeticType> lengths = polyline_segment_lengths(first, last, l);
+        auto it = std::lower_bound(lengths.begin(), lengths.end(), l);
 
-	template <typename Polyline, typename ArithmeticType, typename NumberComparisonPolicy>
-	inline Polyline clip_polyline_front(const Polyline& polyline, ArithmeticType l, const NumberComparisonPolicy& cmp)
-	{
-		typedef point_sequence_traits<Polyline> access;
-		typedef typename point_sequence_traits<Polyline>::point_type point_type;
-		
-		std::vector<ArithmeticType> lengths = polyline_segment_lengths(polyline, l);
-		auto it = std::lower_bound(lengths.begin(), lengths.end(), l);
-		
-		Polyline newPoly;
-		if (it == lengths.end())
-			return std::move(newPoly);
+        Polyline newPoly;
+        if (it == lengths.end())
+            return std::move(newPoly);
 
-		auto index = std::distance(lengths.begin(), it);
-		auto size = access::size(polyline);
-		GEOMETRIX_ASSERT(index + 1 < size);
+        l = *it - l;
 
-		l = *it - l;
-		auto next = index + 1;
+        auto index = std::distance(lengths.begin(), it);
+        PolylineIter pit = std::next(first, index), pnext = std::next(first, index + 1);
+        GEOMETRIX_ASSERT(pnext != last);
 
-		if (cmp.greater_than(l, constants::zero<ArithmeticType>()))
-		{
-			point_type newP{ access::get_point(polyline, next) + l * normalize(access::get_point(polyline, index) - access::get_point(polyline, next)) };
-			if (!numeric_sequence_equals(newP, access::get_point(polyline, next), cmp))
-				newPoly.emplace_back(newP);
-		}
-		else if( ++next < size )
-			newPoly.push_back(access::get_point(polyline, next - 1));
-		
-		for (; next < size; ++next)
-			newPoly.push_back(access::get_point(polyline, next));
+        if (cmp.greater_than(l, constants::zero<ArithmeticType>()))
+        {
+            point_type newP{ *pnext + l * normalize(*pit - *pnext) };
+            if (!numeric_sequence_equals(newP, *pnext, cmp))
+                newPoly.emplace_back(newP);
+        }
+        else if (++pnext != last)
+            newPoly.push_back(*std::prev(pnext));
 
-		return std::move(newPoly);
-	}
-	
-	template <typename Polyline, typename ArithmeticType, typename NumberComparisonPolicy>
-	inline Polyline clip_polyline_back(const Polyline& polyline, ArithmeticType l, const NumberComparisonPolicy& cmp)
-	{
-		typedef point_sequence_traits<Polyline> access;		
-		auto poly = clip_polyline_front<Polyline>(access::rbegin(polyline), access::rend(polyline), l, cmp);
-		std::reverse(access::begin(poly), access::end(poly));
-		return std::move(poly);
-	}
+        for (; pnext != last; ++pnext)
+            newPoly.push_back(*pnext);
+
+        return std::move(newPoly);
+    }
+
+    template <typename Polyline, typename ArithmeticType, typename NumberComparisonPolicy>
+    inline Polyline clip_polyline_front(const Polyline& polyline, ArithmeticType l, const NumberComparisonPolicy& cmp)
+    {
+        typedef point_sequence_traits<Polyline> access;
+        typedef typename point_sequence_traits<Polyline>::point_type point_type;
+
+        std::vector<ArithmeticType> lengths = polyline_segment_lengths(polyline, l);
+        auto it = std::lower_bound(lengths.begin(), lengths.end(), l);
+
+        Polyline newPoly;
+        if (it == lengths.end())
+            return std::move(newPoly);
+
+        auto index = std::distance(lengths.begin(), it);
+        auto size = access::size(polyline);
+        GEOMETRIX_ASSERT(index + 1 < size);
+
+        l = *it - l;
+        auto next = index + 1;
+
+        if (cmp.greater_than(l, constants::zero<ArithmeticType>()))
+        {
+            point_type newP{ access::get_point(polyline, next) + l * normalize(access::get_point(polyline, index) - access::get_point(polyline, next)) };
+            if (!numeric_sequence_equals(newP, access::get_point(polyline, next), cmp))
+                newPoly.emplace_back(newP);
+        }
+        else if( ++next < size )
+            newPoly.push_back(access::get_point(polyline, next - 1));
+
+        for (; next < size; ++next)
+            newPoly.push_back(access::get_point(polyline, next));
+
+        return std::move(newPoly);
+    }
+
+    template <typename Polyline, typename ArithmeticType, typename NumberComparisonPolicy>
+    inline Polyline clip_polyline_back(const Polyline& polyline, ArithmeticType l, const NumberComparisonPolicy& cmp)
+    {
+        typedef point_sequence_traits<Polyline> access;
+        auto poly = clip_polyline_front<Polyline>(access::rbegin(polyline), access::rend(polyline), l, cmp);
+        std::reverse(access::begin(poly), access::end(poly));
+        return std::move(poly);
+    }
 
     template <typename Polyline, typename ArithmeticType, typename NumberComparisonPolicy>
     inline Polyline clip_polyline_ends( const Polyline& poly, const ArithmeticType& distanceFromEndpointToClip, const NumberComparisonPolicy& cmp)
     {
-		return clip_polyline_back(clip_polyline_front(poly, distanceFromEndpointToClip, cmp), distanceFromEndpointToClip, cmp);
+        return clip_polyline_back(clip_polyline_front(poly, distanceFromEndpointToClip, cmp), distanceFromEndpointToClip, cmp);
     }
 
 }//namespace geometrix;

a7816d5b3806f955ad28174eb39abd20c7774221
Brandon Kohn <blkohn@hotmail.com>
2018-10-22 11:25:32 -0400

    Removed constexpr.

--- geometrix/numeric/detail/friend_ordered_field_operators.hpp
+++ geometrix/numeric/detail/friend_ordered_field_operators.hpp
@@ -9,37 +9,42 @@
 #ifndef GEOMETRIX_FRIEND_ORDERED_FIELD_OPERATORS_HPP
 #define GEOMETRIX_FRIEND_ORDERED_FIELD_OPERATORS_HPP
 
+#ifdef GEOMETRIX_TRY_CONSTEXPR
 #include <boost/config.hpp>
+#define GEOMETRIX_CONSTEXPR BOOST_CONSTEXPR
+#else
+#define GEOMETRIX_CONSTEXPR 
+#endif
 
 //! Comparison
-#define GEOMETRIX_FRIEND_LESS_THAN_COMPARABLE_OPERATORS(T, U)                                        \
-BOOST_CONSTEXPR friend bool operator<=(const T& x, const U& y) { return !static_cast<bool>(x > y); } \
-BOOST_CONSTEXPR friend bool operator>=(const T& x, const U& y) { return !static_cast<bool>(x < y); } \
-BOOST_CONSTEXPR friend bool operator>(const U& x, const T& y)  { return y < x; }                     \
-BOOST_CONSTEXPR friend bool operator<(const U& x, const T& y)  { return y > x; }                     \
-BOOST_CONSTEXPR friend bool operator<=(const U& x, const T& y) { return !static_cast<bool>(y < x); } \
-BOOST_CONSTEXPR friend bool operator>=(const U& x, const T& y) { return !static_cast<bool>(y > x); } \
+#define GEOMETRIX_FRIEND_LESS_THAN_COMPARABLE_OPERATORS(T, U)                                            \
+GEOMETRIX_CONSTEXPR friend bool operator<=(const T& x, const U& y) { return !static_cast<bool>(x > y); } \
+GEOMETRIX_CONSTEXPR friend bool operator>=(const T& x, const U& y) { return !static_cast<bool>(x < y); } \
+GEOMETRIX_CONSTEXPR friend bool operator>(const U& x, const T& y)  { return y < x; }                     \
+GEOMETRIX_CONSTEXPR friend bool operator<(const U& x, const T& y)  { return y > x; }                     \
+GEOMETRIX_CONSTEXPR friend bool operator<=(const U& x, const T& y) { return !static_cast<bool>(y < x); } \
+GEOMETRIX_CONSTEXPR friend bool operator>=(const U& x, const T& y) { return !static_cast<bool>(y > x); } \
 /***/
 
-#define GEOMETRIX_FRIEND_LESS_THAN_COMPARABLE_SELF_OPERATORS(T)                                      \
-BOOST_CONSTEXPR friend bool operator>(const T& x, const T& y)  { return y < x; }                     \
-BOOST_CONSTEXPR friend bool operator<=(const T& x, const T& y) { return !static_cast<bool>(y < x); } \
-BOOST_CONSTEXPR friend bool operator>=(const T& x, const T& y) { return !static_cast<bool>(x < y); } \
+#define GEOMETRIX_FRIEND_LESS_THAN_COMPARABLE_SELF_OPERATORS(T)                                          \
+GEOMETRIX_CONSTEXPR friend bool operator>(const T& x, const T& y)  { return y < x; }                     \
+GEOMETRIX_CONSTEXPR friend bool operator<=(const T& x, const T& y) { return !static_cast<bool>(y < x); } \
+GEOMETRIX_CONSTEXPR friend bool operator>=(const T& x, const T& y) { return !static_cast<bool>(x < y); } \
 /***/
 
-#define GEOMETRIX_FRIEND_EQUALITY_COMPARABLE_OPERATORS(T, U)                                         \
-BOOST_CONSTEXPR friend bool operator==(const U& y, const T& x) { return x == y; }                    \
-BOOST_CONSTEXPR friend bool operator!=(const U& y, const T& x) { return !static_cast<bool>(x == y); }\
-BOOST_CONSTEXPR friend bool operator!=(const T& y, const U& x) { return !static_cast<bool>(y == x); }\
+#define GEOMETRIX_FRIEND_EQUALITY_COMPARABLE_OPERATORS(T, U)                                             \
+GEOMETRIX_CONSTEXPR friend bool operator==(const U& y, const T& x) { return x == y; }                    \
+GEOMETRIX_CONSTEXPR friend bool operator!=(const U& y, const T& x) { return !static_cast<bool>(x == y); }\
+GEOMETRIX_CONSTEXPR friend bool operator!=(const T& y, const U& x) { return !static_cast<bool>(y == x); }\
 /***/
 
 #define GEOMETRIX_FRIEND_EQUALITY_COMPARABLE_SELF_OPERATORS(T)           \
-    BOOST_CONSTEXPR friend bool operator!=(const T& x, const T& y)       \
+    GEOMETRIX_CONSTEXPR friend bool operator!=(const T& x, const T& y)   \
     { return !static_cast<bool>(x == y); }                               \
 /***/
 
 #define GEOMETRIX_FRIEND_INCREMENTABLE_OPERATOR(T)                       \
-    BOOST_CONSTEXPR friend T operator++(T& x, int)                       \
+    GEOMETRIX_CONSTEXPR friend T operator++(T& x, int)                   \
     {                                                                    \
         T nrv(x);                                                        \
         ++x;                                                             \
@@ -47,7 +52,7 @@ BOOST_CONSTEXPR friend bool operator!=(const T& y, const U& x) { return !static_
     }                                                                    \
 /***/
 #define GEOMETRIX_FRIEND_DECREMENTABLE_OPERATOR(T)                       \
-    BOOST_CONSTEXPR friend T operator--(T& x, int)                       \
+    GEOMETRIX_CONSTEXPR friend T operator--(T& x, int)                   \
     {                                                                    \
         T nrv(x);                                                        \
         --x;                                                             \
@@ -56,23 +61,23 @@ BOOST_CONSTEXPR friend bool operator!=(const T& y, const U& x) { return !static_
 /***/
 
 //!Arithmetic
-#define GEOMETRIX_FRIEND_BINARY_OPERATOR_COMMUTATIVE( T, U, OP )                         \
-    BOOST_CONSTEXPR friend T operator OP( T lhs, const U& rhs ) { return lhs OP##= rhs; }\
-    BOOST_CONSTEXPR friend T operator OP( const U& lhs, T rhs ) { return rhs OP##= lhs; }\
+#define GEOMETRIX_FRIEND_BINARY_OPERATOR_COMMUTATIVE( T, U, OP )                             \
+    GEOMETRIX_CONSTEXPR friend T operator OP( T lhs, const U& rhs ) { return lhs OP##= rhs; }\
+    GEOMETRIX_CONSTEXPR friend T operator OP( const U& lhs, T rhs ) { return rhs OP##= lhs; }\
 /***/
 
-#define GEOMETRIX_FRIEND_BINARY_OPERATOR_COMMUTATIVE_SELF( T, OP )                       \
-    BOOST_CONSTEXPR friend T operator OP( T lhs, const T& rhs ) { return lhs OP##= rhs; }\
+#define GEOMETRIX_FRIEND_BINARY_OPERATOR_COMMUTATIVE_SELF( T, OP )                           \
+    GEOMETRIX_CONSTEXPR friend T operator OP( T lhs, const T& rhs ) { return lhs OP##= rhs; }\
 /***/
 
-#define GEOMETRIX_FRIEND_BINARY_OPERATOR_NON_COMMUTATIVE( T, U, OP )                     \
-    BOOST_CONSTEXPR friend T operator OP( T lhs, const U& rhs ) { return lhs OP##= rhs; }\
-    BOOST_CONSTEXPR friend T operator OP( const U& lhs, const T& rhs )                   \
-    { return T( lhs ) OP##= rhs; }                                                       \
+#define GEOMETRIX_FRIEND_BINARY_OPERATOR_NON_COMMUTATIVE( T, U, OP )                         \
+    GEOMETRIX_CONSTEXPR friend T operator OP( T lhs, const U& rhs ) { return lhs OP##= rhs; }\
+    GEOMETRIX_CONSTEXPR friend T operator OP( const U& lhs, const T& rhs )                   \
+    { return T( lhs ) OP##= rhs; }                                                           \
 /***/
 
-#define GEOMETRIX_FRIEND_BINARY_OPERATOR_NON_COMMUTATIVE_SELF( T, OP )                   \
-    BOOST_CONSTEXPR friend T operator OP( T lhs, const T& rhs ) { return lhs OP##= rhs; }\
+#define GEOMETRIX_FRIEND_BINARY_OPERATOR_NON_COMMUTATIVE_SELF( T, OP )                       \
+    GEOMETRIX_CONSTEXPR friend T operator OP( T lhs, const T& rhs ) { return lhs OP##= rhs; }\
 /***/
 
 #define GEOMETRIX_FRIEND_IMPLEMENT_BINARY_ARITHMETIC_OPERATORS(T, U)            \
